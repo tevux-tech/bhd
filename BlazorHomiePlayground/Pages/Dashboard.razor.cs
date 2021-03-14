24e1abbb0d1809d480642fa437b4a1b5cee447c1
Matas Girdauskas <matas.girdauskas@lightcon.com>
2021-03-14 19:40:05 +0200

    Working button.

---
+++ BlazorHomiePlayground/Pages/Dashboard.razor.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Connecting;
+using MQTTnet.Client.Disconnecting;
+using MQTTnet.Client.Options;
+using MQTTnet.Client.Receiving;
+
+namespace BlazorHomiePlayground.Pages {
+    partial class Dashboard {
+        private IMqttClient _mqttClient;
+
+        private string _actualState;
+
+        protected override async Task OnInitializedAsync() {
+            var factory = new MqttFactory();
+            _mqttClient = factory.CreateMqttClient();
+
+            _mqttClient.ApplicationMessageReceivedHandler = new MqttApplicationMessageReceivedHandlerDelegate(HandleMessage);
+
+            var clientOptions = new MqttClientOptions { ChannelOptions = new MqttClientWebSocketOptions { Uri = "ws://192.168.2.2:9001/" } };
+
+            _mqttClient.ConnectedHandler = new MqttClientConnectedHandlerDelegate(async e => {
+                Console.WriteLine("### CONNECTED ###");
+
+                await _mqttClient.SubscribeAsync("shellies/shelly1pm-68C63AFADFF9/relay/0");
+
+                Console.WriteLine("### SUBSCRIBED ###");
+            });
+
+            _mqttClient.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(e => {
+                Console.WriteLine("### DISCONNECTED ###");
+            });
+
+
+            try {
+                await _mqttClient.ConnectAsync(clientOptions, CancellationToken.None);
+            } catch (Exception exception) {
+                Console.WriteLine("### CONNECTING FAILED ###" + Environment.NewLine + exception);
+            }
+
+            await base.OnInitializedAsync();
+        }
+
+        private void HandleMessage(MqttApplicationMessageReceivedEventArgs obj) {
+            if (obj.ApplicationMessage.Topic == "shellies/shelly1pm-68C63AFADFF9/relay/0") {
+                var value = Encoding.UTF8.GetString(obj.ApplicationMessage.Payload);
+                _actualState = value;
+                Console.WriteLine(value);
+
+                StateHasChanged();
+            }
+        }
+
+        private async Task TurnOn() {
+            var message = new MqttApplicationMessageBuilder().WithTopic("shellies/shelly1pm-68C63AFADFF9/relay/0/command").WithPayload("on").Build();
+
+            await _mqttClient.PublishAsync(message);
+        }
+
+        private async Task TurnOff() {
+            var message = new MqttApplicationMessageBuilder().WithTopic("shellies/shelly1pm-68C63AFADFF9/relay/0/command").WithPayload("off").Build();
+
+            await _mqttClient.PublishAsync(message);
+        }
+    }
+}
---
+++ BlazorHomiePlayground/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorHomiePlayground {
+    public class Program {
+        public static async Task Main(string[] args) {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
