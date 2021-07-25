a10018117a6c53dec6d3a072b8dad0c41ac177eb
Matas Girdauskas <matas.girdauskas@lightcon.com>
2021-07-25 16:39:40 +0300

    Refactoring NotificationsHub.

--- Redesign/Bhd.Client/NotificationsHub.cs
+++ Redesign/Bhd.Client/NotificationsHub.cs
@@ -1,13 +1,23 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.SignalR.Client;
 
 namespace Bhd.Client {
     public class NotificationsHub {
-        public HubConnection Connection { get; }
+        private readonly HubConnection _connection;
 
         public NotificationsHub(NavigationManager navigationManager) {
-            Connection = new HubConnectionBuilder().WithUrl(navigationManager.ToAbsoluteUri("api/NotificationsHub")).WithAutomaticReconnect(new HubReconnectPolicy()).Build();
-            Connection.StartAsync();
+            _connection = new HubConnectionBuilder().WithUrl(navigationManager.ToAbsoluteUri("api/NotificationsHub")).WithAutomaticReconnect(new HubReconnectPolicy()).Build();
+            _connection.StartAsync();
+        }
+
+        public void OnDeviceStateChanged(Func<string, Task> handler) {
+            _connection.On("DeviceStateChanged", handler);
+        }
+
+        public void OnDevicePropertyChanged(Func<string, Task> handler) {
+            _connection.On("DevicePropertyChanged", handler);
         }
     }
 }
