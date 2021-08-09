0130b7ff676188dd92d81e46427aa51721629de1
Matas Girdauskas <matas.girdauskas@lightcon.com>
2021-08-09 15:30:26 +0300

    GUI for adding dashboards.

--- Bhd.Client/Program.cs
+++ Bhd.Client/Program.cs
@@ -4,6 +4,7 @@ using MudBlazor.Services;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using MudBlazor;
 
 namespace Bhd.Client {
     public class Program {
@@ -18,7 +19,9 @@ namespace Bhd.Client {
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddSingleton<NotificationsHub>();
             builder.Services.AddSingleton<PageHeaderService>();
-            builder.Services.AddMudServices();
+            builder.Services.AddMudServices(config => {
+                config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
+            });
 
             await builder.Build().RunAsync();
         }
