67e60b03ec01fa13a999841f2bd47c6831cb53d4
Matas Girdauskas <matas.girdauskas@lightcon.com>
2021-09-05 16:08:22 +0300

    GUI number picker kind of works.

--- Bhd.Client/Dialogs/NumberPicker.razor.cs
+++ Bhd.Client/Dialogs/NumberPicker.razor.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using MudBlazor;
 
@@ -7,24 +10,71 @@ namespace Bhd.Client.Dialogs {
         [CascadingParameter]
         MudDialogInstance MudDialog { get; set; }
 
-    
         [Parameter]
         public string Unit { get; set; }
 
         [Parameter]
         public double Value { get; set; }
 
+
+        private string _valueString;
+
         private void Cancel() {
             MudDialog.Cancel();
+        }
+
+        protected override void OnParametersSet() {
+            _valueString = Value.ToString(CultureInfo.InvariantCulture);
+            base.OnParametersSet();
         }
 
         private void Set() {
             MudDialog.Close(DialogResult.Ok(Value));
         }
 
+        private void Clear() {
+            Value = 0;
+            _valueString = Value.ToString(CultureInfo.InvariantCulture);
+        }
+
+        private void Backspace() {
+            if (_valueString.Length == 1) {
+                _valueString = "0";
+            } else {
+                _valueString = _valueString.Remove(_valueString.Length - 1);
+            }
+
+            Value = double.Parse(_valueString, CultureInfo.InvariantCulture);
+        }
+
+        private void EnterDigit(int digit) {
+            _valueString += digit.ToString(CultureInfo.InvariantCulture);
+            Value = double.Parse(_valueString, CultureInfo.InvariantCulture);
+        }
+
+        private void Comma() {
+            if (_valueString.Contains('.') == false) {
+                _valueString += ".";
+            }
+
+            Value = double.Parse(_valueString, CultureInfo.InvariantCulture);
+        }
+
+        private void Negate() {
+            if (_valueString.Contains('-')) {
+                _valueString = _valueString.Replace("-", "");
+            } else {
+                _valueString = "-" + _valueString;
+            }
+
+            Value = double.Parse(_valueString, CultureInfo.InvariantCulture);
+        }
+
         public void HandleKeyDown(KeyboardEventArgs obj) {
             if (obj.Key == "Enter") {
-                MudDialog.Close(DialogResult.Ok(Value));
+                Task.Run(() => {
+                    MudDialog.Close(DialogResult.Ok(Value));
+                });
             }
         }
     }
